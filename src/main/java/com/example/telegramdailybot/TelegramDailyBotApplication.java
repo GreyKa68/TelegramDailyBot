7e2c6ffeec875b94121ac60678e5b4772bf8326b
GreyKa68 <you@example.com>
2023-04-16 02:14:16 +0500

    Removed configuration class. Added telegrambots-spring-boot-starter

--- src/main/java/com/example/telegramdailybot/TelegramDailyBotApplication.java
+++ src/main/java/com/example/telegramdailybot/TelegramDailyBotApplication.java
@@ -2,12 +2,17 @@ package com.example.telegramdailybot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@ComponentScan(basePackages = {
+        "com.example.telegramdailybot",
+        "org.telegram.telegrambots"
+})
 public class TelegramDailyBotApplication {
 
     public static void main(String[] args) {
